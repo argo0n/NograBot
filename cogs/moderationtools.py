55c453ff59c50abe59b7962ac3da93a2542a5619
argo0n <laiyeqi@gmail.com>
2021-03-22 13:39:17 +0800

    fix datetie glitch

--- cogs/moderationtools.py
+++ cogs/moderationtools.py
@@ -1,6 +1,7 @@
 from discord.ext import commands
 import discord
-from datetime import datetime, time, date, timezone
+from datetime import datetime
+import time
 import math
 
 start_time = time.time()

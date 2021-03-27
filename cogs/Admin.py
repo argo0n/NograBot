5051a1c09b7dc4d164b70474d659064055b09035
argo0n <laiyeqi@gmail.com>
2021-03-27 22:43:53 +0800

    Remove redudant code (eval command), renamed file for cog usage

--- cogs/Admin.py
+++ cogs/Admin.py
@@ -20,11 +20,8 @@ import datetime
 from collections import Counter
 
 class Admin(commands.Cog):
-
     def __init__(self, client):
         self.client = client
-        self._last_result = None
-        self.sessions = set()
 
     def cleanup_code(self, content):
         """Automatically removes code blocks from the code."""
@@ -40,7 +37,7 @@ class Admin(commands.Cog):
         print('Cog Admin loaded')'''
 
     @commands.command(pass_context=True)
-    #@commands.is_owner()
+    @commands.is_owner()
     async def edit(self, ctx,messageid:int=None, channel:discord.TextChannel=None,*, newmessage=None):
         if messageid is None:
             await ctx.send("You didn't give me a Message ID to edit.")
@@ -60,60 +57,17 @@ class Admin(commands.Cog):
             else:
                 await ctx.send("That message was not sent by me, I can't edit it.")
 
-    @commands.command(pass_context=True, hidden=True, name='eval')
-    async def _eval(self, ctx, *, body: str):
-        """Evaluates a code"""
-
-        env = {
-            'bot': self.client,
-            'ctx': ctx,
-            'channel': ctx.channel,
-            'author': ctx.author,
-            'guild': ctx.guild,
-            'message': ctx.message,
-            '_': self._last_result
-        }
-
-        env.update(globals())
-
-        body = self.cleanup_code(body)
-        stdout = io.StringIO()
-
-        to_compile = f'async def func():\n{textwrap.indent(body, "  ")}'
-
-        try:
-            exec(to_compile, env)
-        except Exception as e:
-            return await ctx.send(f'```py\n{e.__class__.__name__}: {e}\n```')
-
-        func = env['func']
-        try:
-            with redirect_stdout(stdout):
-                ret = await func()
-        except Exception as e:
-            value = stdout.getvalue()
-            await ctx.send(f'```py\n{value}{traceback.format_exc()}\n```')
-        else:
-            value = stdout.getvalue()
-            try:
-                await ctx.message.add_reaction('\u2705')
-            except:
-                pass
-
-            if ret is None:
-                if value:
-                    await ctx.send(f'```py\n{value}\n```')
-            else:
-                self._last_result = ret
-                await ctx.send(f'```py\n{value}{ret}\n```')
+    '''@commands.command(pass_context=True)
+    async def dm(self, ctx, *, message):'''
 
     @edit.error
     async def edit_error(self, ctx, error):
         await ctx.send(f"```diff\n- Error encountered!\n# erorr:\n+ {error}```")
 
-    @_eval.error
+
+    '''@_eval.error
     async def _eval_error(self, ctx, error):
-        await ctx.send(f"```diff\n- Error encountered!\n# erorr:\n+ {error}```")
+        await ctx.send(f"```diff\n- Error encountered!\n# erorr:\n+ {error}```")'''
 
 def setup(client):
     client.add_cog(Admin(client))

38cfec69e6436604aa752e403bf61e416f1619c3
argo0n <laiyeqi@gmail.com>
2021-04-02 21:35:36 +0800

    Revert "april fools be like"
    This reverts commit 5019e4bf2bc68f09d6d8fd0e046607d25b21b3c7.

--- nogra.py
+++ nogra.py
@@ -375,7 +375,6 @@ async def nosee(ctx):
     if ctx.author.id in [650647680837484556]:
         for c in ctx.guild.channels:
             await c.set_permissions(ctx.guild.default_role, view_channel=False)
-        await ctx.send("No one can see all the channels now (Unless they have admin perms)")
     else:
         ctx.send("imagine not being argon lol")
 

3ac07df4f292cbf9996e17032ead802a6fb4864a
argo0n <laiyeqi@gmail.com>
2021-04-17 17:18:27 +0800

    change footer

--- nogra.py
+++ nogra.py
@@ -63,6 +63,7 @@ async def on_guild_join(guild):
 
 async def on_message(message):
 
+
     argon = message.guild.get_member(650647680837484556)
     if argon.mentioned_in(message) and message.author != client.user:
         date_time_obj = datetime.datetime.strptime('16/04/21 06:00:00', '%d/%m/%y %H:%M:%S')
@@ -76,7 +77,7 @@ async def on_message(message):
         joinembed.set_author(name=f"{argon.name}", icon_url=str(argon.avatar_url))
         joinembed.add_field(name=":(",value="having exams, won't respond to you as quickly <a:nyakiss:832467845417009162>\n- argon",inline=True)
         joinembed.set_thumbnail(url="https://cdn.discordapp.com/emojis/694973517434978445.png?v=1")
-        joinembed.set_footer(text=f"Argon has been offline since {formatofdatesinceafk} UTC, and he has been AFK for {afksince}")
+        joinembed.set_footer(text=f"Argon has been AFK for {afksince}. Also this is __not__ a afk command lmao")
         await message.channel.send(embed=joinembed)
 
     if message.channel.id == 821042728849768478:

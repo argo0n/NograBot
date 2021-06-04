e6722e93dcb49d4ee31e3ebf3a09ca0a025c738e
argo0n <laiyeqi@gmail.com>
2021-06-04 15:42:48 +0800

    redundant logchannel variable

--- cogs/Abuse.py
+++ cogs/Abuse.py
@@ -12,6 +12,7 @@ import discord
 from discord.ext import commands
 import asyncio
 import postbin, traceback
+import math
 
 def gettraceback(error):
     etype = type(error)
@@ -20,6 +21,47 @@ def gettraceback(error):
     traceback_text = ''.join(lines)
     return traceback_text
 
+
+def secondstotiming(seconds):
+    seconds = round(seconds)
+    if seconds < 60:
+        secdisplay = "s" if seconds != 1 else ""
+        return f"{seconds} second{secdisplay}"
+    minutes = math.trunc(seconds / 60)
+    if minutes < 60:
+        seconds = seconds - minutes * 60
+        mindisplay = "s" if minutes != 1 else ""
+        secdisplay = "s" if seconds != 1 else ""
+        return f"{minutes} minute{mindisplay} and {seconds} second{secdisplay}"
+    hours = math.trunc(minutes / 60)
+    if hours < 24:
+        minutes = minutes - hours * 60
+        seconds = seconds - minutes * 60 - hours * 60 * 60
+        hdisplay = "s" if hours != 1 else ""
+        mindisplay = "s" if minutes != 1 else ""
+        secdisplay = "s" if seconds != 1 else ""
+        return f"{hours} hour{hdisplay}, {minutes} minute{mindisplay} and {seconds} second{secdisplay}"
+    days = math.trunc(hours / 24)
+    if days < 7:
+        hours = hours - days * 24
+        minutes = minutes - hours * 60
+        seconds = seconds - minutes * 60 - hours * 60 * 60
+        ddisplay = "s" if days != 1 else ""
+        hdisplay = "s" if hours != 1 else ""
+        mindisplay = "s" if minutes != 1 else ""
+        secdisplay = "s" if seconds != 1 else ""
+        return f"{days} day{ddisplay}, {hours} hour{hdisplay}, {minutes} minute{mindisplay} and {seconds} second{secdisplay}"
+    weeks = math.trunc(days / 7)
+    days = days - weeks * 7
+    hours = hours - days * 24
+    minutes = minutes - hours * 60
+    seconds = seconds - minutes * 60 - hours * 60 * 60
+    wdisplay = "s" if weeks != 1 else ""
+    ddisplay = "s" if days != 1 else ""
+    hdisplay = "s" if hours != 1 else ""
+    mindisplay = "s" if minutes != 1 else ""
+    secdisplay = "s" if seconds != 1 else ""
+    return f"{weeks} week{wdisplay}, {days} day{ddisplay}, {hours} hour{hdisplay}, {minutes} minute{mindisplay} and {seconds} second{secdisplay}"
 
 class Abuse(commands.Cog):
 
